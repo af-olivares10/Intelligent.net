4658c9c78dcf63e42f50e1ba2113bdc77028721b
Andrés <af.olivares10@uniandes.edu.co>
2018-11-21 14:32:53 -0500

    first commit

---
+++ precache-manifest.6647a7c77e05c770d7c5c7efdfa11b2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d63685c1fd0e875c4b0bdba314bc751a",
+    "url": "/static/media/logo_text.d63685c1.PNG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee5be43e40829d4a7c4c",
+    "url": "/static/js/main.ee5be43e.chunk.js"
+  },
+  {
+    "revision": "6ba77729b4519f726455",
+    "url": "/static/js/1.6ba77729.chunk.js"
+  },
+  {
+    "revision": "ee5be43e40829d4a7c4c",
+    "url": "/static/css/main.dfac9c63.chunk.css"
+  },
+  {
+    "revision": "6ba77729b4519f726455",
+    "url": "/static/css/1.3ebfd61a.chunk.css"
+  },
+  {
+    "revision": "3c96a3f9b765316944d1c868bf6d6cd9",
+    "url": "/index.html"
+  }
+];
